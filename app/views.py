f0d0095376de01b14f5016e6cf4e6975b42b9d4d
Julio Fuentes <jefuente@uc.cl>
2021-08-27 23:42:38 -0400

    Actualizacion validaciones

--- app/views.py
+++ app/views.py
@@ -20,6 +20,7 @@ def agregar(request):
 def editar(request, id):
     context = {
         'id' : id,
+        'prog': Show.objects.get(id=id),
     }
     return render(request, 'editar.html', context)
     
@@ -69,16 +70,46 @@ def crear_show(request):
 def update(request, id):
     print(request.POST)
 
-    tv_show = Show.objects.get(id=request.POST['identificador'])
-    tv_show.title=request.POST['title']
-    tv_show.network=request.POST['network']
-    tv_show.release_date=request.POST['release']
-    tv_show.description=request.POST['description'] 
-    tv_show.save()
-    return redirect(f"/shows/{tv_show.id}")
+    errores = Show.objects.validador(request.POST)
+    print(errores)
+
+    if len(errores) > 0:
+        # si el diccionario de errores contiene algo, recorra cada par clave-valor y cree un mensaje flash
+        for key, value in errores.items():
+            messages.error(request, value)
+
+        context = {
+        'id' : id,
+        'prog': Show.objects.get(id=id),
+        }
+
+        request.session['editar_title']=request.POST['title']
+        request.session['editar_network']=request.POST['network']
+        request.session['editar_release']=request.POST['release']
+        request.session['editar_description']=request.POST['description'] 
+
+        # redirigir al usuario al formulario para corregir los errores
+        return render(request, 'editar.html', context)
+
+    else:
+
+        request.session['editar_title']=''
+        request.session['editar_network']=''
+        request.session['editar_release']=''
+        request.session['editar_description']=''
+
+        tv_show = Show.objects.get(id=request.POST['identificador'])
+        tv_show.title=request.POST['title']
+        tv_show.network=request.POST['network']
+        tv_show.release_date=request.POST['release']
+        tv_show.description=request.POST['description'] 
+        tv_show.save()
+        messages.success(request,"Exito en editar el título "+tv_show.title)
+        return redirect(f"/shows/{tv_show.id}")
 
 def borrar(request, id):
     id : id
     programa = Show.objects.get(id=id)
     programa.delete()
+    messages.success(request,"Exito en eliminar el título "+programa.title)
     return redirect("/shows")
